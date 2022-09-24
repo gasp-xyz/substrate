d250fe24a334c3e5bcebde6cc67b70a846731dff
Mateusz Nowakowski <mat13mn@gmail.com>
2022-09-24 10:02:00 +0200

    when prevalidating ignore only future txs

--- frame/system/src/extensions/check_nonce.rs
+++ frame/system/src/extensions/check_nonce.rs
@@ -81,13 +81,16 @@ where
 	) -> Result<(), TransactionValidityError> {
 		let mut account = crate::Account::<T>::get(who);
 
-		if !crate::TxPrevalidation::<T>::get() && self.0 != account.nonce {
-			return Err(if self.0 < account.nonce {
-				InvalidTransaction::Stale
+		if self.0 != account.nonce {
+			if self.0 < account.nonce {
+				return Err(InvalidTransaction::Stale.into())
 			} else {
-				InvalidTransaction::Future
+				if crate::TxPrevalidation::<T>::get() {
+					// ignore future txs when prevalidating
+				} else {
+					return Err(InvalidTransaction::Future.into())
+				}
 			}
-			.into())
 		}
 		account.nonce += T::Index::one();
 		crate::Account::<T>::insert(who, account);
